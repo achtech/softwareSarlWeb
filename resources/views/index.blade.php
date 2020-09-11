2fff94bb56b60048debde8b53add90ee09eaf40a
Noura bouchbaat <noura.bouchbaat@exo-it.com>
2020-09-11 18:30:48 +0200

    commit customer

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -324,7 +324,7 @@
 
         <div class="row">
           <div class="col-lg-6" data-aos="fade-right">
-            <img src="../assets/img/about11.jpg" class="img-fluid" alt="">
+            <img src="../assets/img/aboutUs.jpg" class="img-fluid" alt="">
           </div>
           <div class="col-lg-6 pt-4 pt-lg-0 content" data-aos="fade-left">
              <p class="font-italic">
@@ -430,28 +430,28 @@
              
               <div class="row">
     
-                <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
-                  <img src="../assets/img/clients/client-1.png" class="img-fluid" alt="">
+              <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
+                  <img src="assets/img/clients/customer5.png" class="img-fluid" alt="">
                 </div>
     
                 <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
-                  <img src="../assets/img/clients/client-2.png" class="img-fluid" alt="">
+                  <img src="assets/img/clients/customer6.png" class="img-fluid" alt="">
                 </div>
     
                 <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
-                  <img src="../assets/img/clients/client-3.png" class="img-fluid" alt="">
+                  <img src="assets/img/clients/customer1.png" class="img-fluid" alt="">
                 </div>
     
                 <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
-                  <img src="../assets/img/clients/client-4.png" class="img-fluid" alt="">
+                  <img src="assets/img/clients/customer2.png" class="img-fluid" alt="">
                 </div>
     
                 <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
-                  <img src="../assets/img/clients/client-5.png" class="img-fluid" alt="">
+                  <img src="assets/img/clients/customer3.png" class="img-fluid" alt="">
                 </div>
     
                 <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
-                  <img src="../assets/img/clients/client-6.png" class="img-fluid" alt="">
+                  <img src="assets/img/clients/customer7.png" class="img-fluid" alt="">
                 </div>
     
               </div>
